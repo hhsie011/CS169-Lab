2be7c0a721597a6a85477804da2449a8e6e94235
Hsiang-Yin Hsieh <hhsie011@wch132-21.cs.ucr.edu>
2022-06-30 23:09:06 -0700

    initial commit

---
+++ ns-allinone-3.25/ns-3.25/scratch/first-exe.cc
@@ -0,0 +1,93 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include <vector>
+
+using namespace ns3;
+
+int NUM_CLIENTS = 3;
+
+NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+
+int
+main (int argc, char *argv[])
+{
+  Time::SetResolution (Time::NS);
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+  NodeContainer nodes;
+  nodes.Create (NUM_CLIENTS+1);
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("4ms"));
+
+  InternetStackHelper stack;
+  stack.Install (nodes);
+ 
+  Ipv4AddressHelper address1;
+  address1.SetBase ("10.1.1.0", "255.255.255.0");
+
+  NetDeviceContainer device1;
+  device1 = pointToPoint.Install (nodes.Get(0), nodes.Get(1));
+  Ipv4InterfaceContainer interface1 = address1.Assign(device1);
+
+  Ipv4AddressHelper address2;
+  address2.SetBase ("10.1.2.0", "255.255.255.0");
+  
+  NetDeviceContainer device2;
+  device2 = pointToPoint.Install (nodes.Get(0), nodes.Get(2));    
+  Ipv4InterfaceContainer interface2 = address2.Assign (device2);
+
+  Ipv4AddressHelper address3;
+  address3.SetBase("10.1.3.0", "255.255.255.0");
+
+  NetDeviceContainer device3;
+  device3 = pointToPoint.Install (nodes.Get(0), nodes.Get(3));
+  Ipv4InterfaceContainer interface3 = address3.Assign (device3);
+
+  std::vector<Ipv4InterfaceContainer> interfaces;
+  interfaces.push_back(interface1); 
+  interfaces.push_back(interface2);
+  interfaces.push_back(interface3);  
+
+  UdpEchoServerHelper echoServer (9);
+
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (0));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+  for (int i = 0; i < NUM_CLIENTS; ++i) {   
+    UdpEchoClientHelper echoClient (interfaces[i].GetAddress(0), 9);
+    echoClient.SetAttribute ("MaxPackets", UintegerValue (4));
+    echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+    echoClient.SetAttribute ("PacketSize", UintegerValue (16384)); 
+
+    ApplicationContainer clientApps = echoClient.Install (nodes.Get (i+1));
+    clientApps.Start (Seconds (2.0+i));
+    clientApps.Stop (Seconds (20.0));
+  }
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
